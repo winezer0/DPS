b95ad9cd2d669801710e37d1ece9f374dea79e57
WINEZERO <kissloo@foxmail.com>
2023-08-03 09:24:04 +0800

    readme更新

--- setting_http.py
+++ setting_http.py
@@ -40,15 +40,15 @@ def init_custom(config):
     ##################################################################
     # 默认请求头设置
     config[GB_REQ_HEADERS] = {
-        # 'Host': 'testphp.vulnweb.com',
+        # 'Host': 'testphp.vulnweb.com',    # 默认会自动添加请求HOST
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Cache-Control': 'max-age=0',
         'Content-Type': 'application/x-www-form-urlencoded',
-        # 'Origin': 'http://testphp.vulnweb.com',
-        # 'Referer': 'http://testphp.vulnweb.com/',
-        'Upgrade-Insecure-Requests': '1',
+        # 'Origin': 'http://testphp.vulnweb.com',  # 默认会自动添加请求URL
+        # 'Referer': 'http://testphp.vulnweb.com/',  # 默认会自动添加请求URL
+        # 'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
         # 'Transfer-Encoding': 'identity',
         'Connection': 'Close',
