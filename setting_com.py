75c1ca658b9af2c275e7ccd633073a871fcbedd5
WINEZERO <kissloo@foxmail.com>
2023-08-03 10:11:50 +0800

    v0.2.1

--- setting_com.py
+++ setting_com.py
@@ -23,7 +23,7 @@ def init_common(config):
     config[GB_RUN_TIME] = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
     ##################################################################
     # 版本号配置
-    config[GB_VERSION] = "Ver 0.2.0 2023-8-03 01:30"
+    config[GB_VERSION] = "Ver 0.2.1 2023-8-03 12:30"
     ##################################################################
     # 是否显示DEBUG级别信息,默认False
     config[GB_DEBUG_FLAG] = False
